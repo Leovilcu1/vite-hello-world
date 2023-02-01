ffc7cb19bcb7607a1227a075288f7a1ab3267ce9
LeonidVilcu <vilcu.leo@mail.ru>
2023-02-01 23:28:03 +0100

    first commit

---
+++ vite-project/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import AppTitle from './components/AppTitle.vue'
+export default {
+  components: {
+    AppTitle
+  },
+  data() {
+    return {
+      count: 0,
+
+    }
+  }
+}
+</script>
+
+<template>
+<AppTitle/>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
---
+++ vite-project/src/components/AppTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+      AppTitle: "La mia prima app con vite"
+    }
+  }
+}
+</script>
+
+<template>
+<h1>{{ AppTitle }}</h1>
+</template>
+
+<style scoped>
+button {
+  font-weight: bold;
+}
+</style>
